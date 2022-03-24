ea4c0cd2376f1301ddde349a1c8bb07e8e33588b
Emanuel dos Santos <emanuel_s.c@hotmail.com>
2022-03-23 22:00:17 -0300

    Criando componente de tempo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemporizadorComponent } from './temporizador/temporizador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemporizadorComponent
   ],
   imports: [
     BrowserModule,
